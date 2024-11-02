63a23801669dda34c4d175695c9ab596f61b6d30
fengscarleteyes <15916435511@163.com>
2024-11-02 22:59:26 +0800

    update

--- lua/config/key.lua
+++ lua/config/key.lua
@@ -1,2 +1,4 @@
 -- Example
 -- vim.keymap.set("n", "<F7>", vim.lsp.buf.hover) -- hover
+-- vim.keymap.set('n', '<M-Tab>', function () vim.cmd('NeoTermToggle') end)
+-- vim.keymap.set('t', '<M-Tab>', function () vim.cmd('NeoTermEnterNormal') end)
