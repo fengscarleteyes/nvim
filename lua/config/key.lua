8906478eeb91951221279144dd6d3c3d3fe1eb26
fengscarleteyes <15916435511@163.com>
2024-11-10 21:44:02 +0800

    update

--- lua/config/key.lua
+++ lua/config/key.lua
@@ -2,3 +2,5 @@
 -- vim.keymap.set("n", "<F7>", vim.lsp.buf.hover) -- hover
 -- vim.keymap.set('n', '<M-Tab>', function () vim.cmd('NeoTermToggle') end)
 -- vim.keymap.set('t', '<M-Tab>', function () vim.cmd('NeoTermEnterNormal') end)
+
+-- vim.keymap.set("n", "<F7>", vim.lsp.buf.hover)
--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,6 +1,13 @@
 return {
   "numToStr/FTerm.nvim",
-  opts = { cmd = "powershell" }, -- windows
+  opts = function()
+    if vim.fn.has("win32") == 1 then
+      return { cmd = "powershell" }
+    elseif vim.fn.has("unix") == 1 then
+      return { cmd = "bash" }
+    end
+  end,
+  -- opts = { cmd = "powershell" }, -- windows
   -- opts = { cmd = "bash" }, -- linux
   -- stylua: ignore
   keys = {
