b2394b2b8ce34c2230eb83744c18ffe1a85654a2
fengscarleteyes <15916435511@163.com>
2024-11-12 22:18:26 +0800

    update

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -1,21 +1,5 @@
 return {
   "CRAG666/code_runner.nvim",
-  -- opts = {
-  --   filetype_path = function()
-  --     if vim.fn.has("win32") == 1 then
-  --       return vim.fn.expand("~/AppData/Local/nvim/code_runner.json")
-  --     elseif vim.fn.has("unix") == 1 then
-  --       return vim.fn.expand("~/.config/nvim/code_runner.json")
-  --     end
-  --   end,
-  --   project_path = function()
-  --     if vim.fn.has("win32") == 1 then
-  --       return vim.fn.expand("~/AppData/Local/nvim/project_manager.json")
-  --     elseif vim.fn.has("unix") == 1 then
-  --       return vim.fn.expand("~/.config/nvim/project_manager.json")
-  --     end
-  --   end,
-  -- },
   opts = function()
     if vim.fn.has("win32") == 1 then
       return {
