0e2d77080503394e803e4aa34d4400dcbd5e7eba
fengscarleteyes <15916435511@163.com>
2024-11-21 22:17:37 +0800

    update

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,10 +1,10 @@
--- https://github.com/Exafunction/codeium.vim
+vim.g.codeium_disable_bindings = 1
+
 return {
   "Exafunction/codeium.vim",
   -- enabled = false,
   event = "BufEnter",
   -- Codeium Auth to set up the plugin and start using Codeium
-  config = function()
-    vim.g.codeium_disable_bindings = 1
-  end,
+  -- https://github.com/Exafunction/codeium.vim
+  -- vim.g.codeium_disable_bindings = 1
 }
