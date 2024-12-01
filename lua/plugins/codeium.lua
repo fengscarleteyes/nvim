e77ad48e76873e4ee0bdeae4fdb5a9f1e06a8460
fengscarleteyes <15916435511@163.com>
2024-12-01 10:27:59 +0800

    update

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,19 +2,19 @@ vim.g.codeium_disable_bindings = 1
 vim.g.codeium_enabled = true
 
 return {
-  "Exafunction/codeium.vim",
-  enabled = false,
-  -- enabled = function()
-  --   if vim.fn.has("win32") == 1 then
-  --     return false
-  --   elseif vim.fn.has("unix") == 1 then
-  --     return true
-  --   end
-  -- end,
-  event = "BufEnter",
-  -- Codeium Auth to set up the plugin and start using Codeium
-  -- https://github.com/Exafunction/codeium.vim
-  -- vim.g.codeium_disable_bindings = 1
+  "Exafunction/codeium.nvim",
+  enabled = function()
+    if vim.fn.has("win32") == 1 then
+      return false
+    elseif vim.fn.has("unix") == 1 then
+      return true
+    end
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup({})
+  end,
 }
-
--- the plugin in windows will show some error messages
