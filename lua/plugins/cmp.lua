f6a5403e7e74e6bbc053e44cc0da781acee208d5
fengscarleteyes <15916435511@163.com>
2024-12-01 11:50:58 +0800

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,8 +7,6 @@ return {
     "neovim/nvim-lspconfig",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-cmdline",
-    -- { "L3MON4D3/LuaSnip", version = "v2.*" },
-    -- "saadparwaiz1/cmp_luasnip",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-emoji",
@@ -17,11 +15,6 @@ return {
     local cmp = require("cmp")
     cmp.setup({
       preselect = cmp.PreselectMode.None,
-      -- snippet = {
-      --   expand = function(args)
-      --     require("luasnip").lsp_expand(args.body)
-      --   end,
-      -- },
       mapping = {
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         ["<Tab>"] = cmp.mapping.select_next_item(),
@@ -34,7 +27,6 @@ return {
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "cmdline" },
-        -- { name = "luasnip", option = { show_autosnippets = true } },
         { name = "emoji" },
       }, {
         { name = "buffer" },
