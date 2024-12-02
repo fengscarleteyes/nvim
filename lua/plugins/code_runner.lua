8d2c53a5614e1255d2e4909ddf599a4c61391872
fengscarleteyes <15916435511@163.com>
2024-12-02 21:59:22 +0800

    update

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -20,7 +20,8 @@
 return {
   "CRAG666/code_runner.nvim",
   opts = {
-      filetype_path = vim.fs.joinpath(vim.fn.stdpath("config"),"code_runner.json"),
-      project_path = vim.fs.joinpath(vim.fn.stdpath("config"),"project_manager.json"),
-    }
+    filetype_path = vim.fs.joinpath(vim.fn.stdpath("config"), "code_runner.json"),
+    project_path = vim.fs.joinpath(vim.fn.stdpath("config"), "project_manager.json"),
+  },
 }
+
