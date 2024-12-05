eb966e907d9616c162daa33d07dbe8f61f184915
fengscarleteyes <15916435511@163.com>
2024-12-05 21:52:48 +0800

    update

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -1,49 +1,55 @@
 local wk = require("which-key")
 
-local flash = require("flash")
+-- require:
+local use_oil = require("oil")
+local use_flash = require("flash")
+local use_noice = require("noice")
+local use_fterm = require("FTerm")
+local use_telescope = require("telescope")
+local use_telescope_builtin = require("telescope.builtin")
+
 -- stylua: ignore start
 wk.add({
   { "<leader>", group = "leader keys" }, -- group
 
-  { "<leader><Space>", mode = "n", function() require("noice").cmd("dismiss") end, desc = "noice dismiss" },
+  { "<leader><Space>", mode = "n", function() use_noice.cmd("dismiss") end, desc = "noice dismiss" },
   -- TODO: add noicedisable key and toggle enable
   --
-  { "<A-t>", mode = { "t", "n" }, function() require("FTerm").toggle() end, desc = "FTerm toggle" },
+  { "<A-t>", mode = { "t", "n" }, function() use_fterm.toggle() end, desc = "FTerm toggle" },
   { "<leader>w", mode = "n", "<cmd>bd<CR>", desc = "Buffer Delete" },
 
-  { "s", mode = { "n", "x", "o" }, function() flash.jump()              end, desc = "Flash" },
-  { "S", mode = { "n", "x", "o" }, function() flash.treesitter()        end, desc = "Flash Treesitter" },
-  { "r", mode = { "o" },           function() flash.remote()            end, desc = "Remote Flash" }, -- in omap"y, d, c ..."
-  { "R", mode = { "o", "x" },      function() flash.treesitter_search() end, desc = "Treesitter Search" },
-  { "<c-s>", mode = { "c" },       function() flash.toggle()            end, desc = "Toggle Flash Search" }, -- in "/" search mode toggle flash
+  { "s",     mode = { "n", "x", "o" }, function() use_flash.jump()              end, desc = "Flash"               },
+  { "S",     mode = { "n", "x", "o" }, function() use_flash.treesitter()        end, desc = "Flash Treesitter"    },
+  { "r",     mode = { "o"           }, function() use_flash.remote()            end, desc = "Remote Flash"        }, -- in omap"y, d, c ..."
+  { "R",     mode = { "o", "x"      }, function() use_flash.treesitter_search() end, desc = "Treesitter Search"   },
+  { "<c-s>", mode = { "c"           }, function() use_flash.toggle()            end, desc = "Toggle Flash Search" }, -- in "/" search mode toggle flash
 
   { "<leader>t", group = "File | Tree" }, -- group
-  { "<leader>tb", mode = "n", "<cmd>Neotree source=buffers reveal=true position=float action=focus toggle=true<CR>", desc = "NeoTree | Buffers" },
+  { "<leader>tb", mode = "n", "<cmd>Neotree source=buffers    reveal=true position=float action=focus toggle=true<CR>", desc = "NeoTree | Buffers"    },
   { "<leader>tg", mode = "n", "<cmd>Neotree source=git_status reveal=true position=float action=focus toggle=true<CR>", desc = "NeoTree | Git Status" },
   { "<leader>tf", mode = "n", "<cmd>Neotree source=filesystem reveal=true position=float action=focus toggle=true<CR>", desc = "NeoTree | Filesystem" },
 
   { "<leader>f", group = "File | Find" }, -- group
-  { "<leader>fo", mode = "n", function() require("oil").toggle_float() end, desc = "Oil toggle" },
-  { "<leader>fn", mode = "n", function() require("lua.plugins.editor_telescope").extensions.nerdy.nerdy() end, desc = "Oil toggle" },
-  { "<leader>ff", mode = "n", function() require("telescope.builtin").find_files() end, desc = "telescope find files" },
-  { "<leader>fb", mode = "n", function() require("telescope.builtin").buffers() end, desc = "telescope buffers" },
-  { "<leader>fg", mode = "n", function() require("telescope.builtin").live_grep() end, desc = "telescope live grep" },
-  { "<leader>fh", mode = "n", function() require("telescope.builtin").help_tags() end, desc = "telescope help tags" },
+  { "<leader>fo", mode = "n", function() use_oil.toggle_float()                 end, desc = "Oil toggle"           },
+  { "<leader>fn", mode = "n", function() use_telescope.extensions.nerdy.nerdy() end, desc = "telescope find nerd"  },
+  { "<leader>ff", mode = "n", function() use_telescope_builtin.find_files()     end, desc = "telescope find files" },
+  { "<leader>fb", mode = "n", function() use_telescope_builtin.buffers()        end, desc = "telescope buffers"    },
+  { "<leader>fg", mode = "n", function() use_telescope_builtin.live_grep()      end, desc = "telescope live grep"  },
+  { "<leader>fh", mode = "n", function() use_telescope_builtin.help_tags()      end, desc = "telescope help tags"  },
 
   { "<leader>r", group = "RunCode" },
-  { "<leader>rr", mode = "n", "<Cmd>RunCode<CR>", desc = "RunCode", noremap = true, silent = false },
-  { "<leader>rf", mode = "n", "<Cmd>RunFile tab<CR>", desc = "RunFile tab", noremap = true, silent = false },
-  { "<leader>rp", mode = "n", "<Cmd>RunProject tab<CR>", desc = "RunProject", noremap = true, silent = false },
-  { "<leader>rc", mode = "n", "<Cmd>RunClose<CR>", desc = "RunClose", noremap = true, silent = false },
-  { "<leader>ref", mode = "n", "<Cmd>CRFiletype<CR>", desc = "CRFiletype", noremap = true, silent = false },
-  { "<leader>rep", mode = "n", "<Cmd>CRProjects<CR>", desc = "CRProjects", noremap = true, silent = false },
+  { "<leader>rr",  mode = "n", "<Cmd>RunCode<CR>",        desc = "RunCode",     noremap = true, silent = false },
+  { "<leader>rf",  mode = "n", "<Cmd>RunFile tab<CR>",    desc = "RunFile tab", noremap = true, silent = false },
+  { "<leader>rp",  mode = "n", "<Cmd>RunProject tab<CR>", desc = "RunProject",  noremap = true, silent = false },
+  { "<leader>rc",  mode = "n", "<Cmd>RunClose<CR>",       desc = "RunClose",    noremap = true, silent = false },
+  { "<leader>ref", mode = "n", "<Cmd>CRFiletype<CR>",     desc = "CRFiletype",  noremap = true, silent = false },
+  { "<leader>rep", mode = "n", "<Cmd>CRProjects<CR>",     desc = "CRProjects",  noremap = true, silent = false },
 
   { "<leader>s", group = "Surround" },
-  { "<leader>ss", mode = "n", "<Plug>(nvim-surround-normal-cur)", desc = "nvim-surround-normal-cur", noremap = true, silent = false },
-  { "<leader>sd", mode = "n", "<Plug>(nvim-surround-delete)", desc = "nvim-surround-delete", noremap = true, silent = false },
+  { "<leader>ss", mode = "n", "<Plug>(nvim-surround-normal-cur)",      desc = "nvim-surround-normal-cur",      noremap = true, silent = false },
+  { "<leader>sd", mode = "n", "<Plug>(nvim-surround-delete)",          desc = "nvim-surround-delete",          noremap = true, silent = false },
   { "<leader>sl", mode = "n", "<Plug>(nvim-surround-normal-cur-line)", desc = "nvim-surround-normal-cur-line", noremap = true, silent = false },
-  { "<leader>sr", mode = "n", "<Plug>(nvim-surround-change)", desc = "nvim-surround-change", noremap = true, silent = false },
-
+  { "<leader>sr", mode = "n", "<Plug>(nvim-surround-change)",          desc = "nvim-surround-change",          noremap = true, silent = false },
   -- <Plug>(nvim-surround-insert)
   -- <Plug>(nvim-surround-insert-line)
   -- <Plug>(nvim-surround-normal)
