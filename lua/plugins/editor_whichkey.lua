084c75865deda0426e994994b465913a1391798f
fengscarleteyes <15916435511@163.com>
2024-12-10 10:51:49 +0800

    update

--- lua/plugins/editor_whichkey.lua
+++ lua/plugins/editor_whichkey.lua
@@ -2,13 +2,35 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    win = {
+      -- don't allow the popup to overlap with the cursor
+      no_overlap = true,
+      width = 50,
+      height = { min = 4, max = 25 },
+      col = 1,
+      -- row = math.huge,
+      border = "single",
+      padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
+      -- Additional vim.wo and vim.bo options
+      bo = {},
+      wo = {
+        -- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+      },
+    },
+    layout = {
+      width = { min = 20 }, -- min and max width of the columns
+      spacing = 3, -- spacing between columns
+    },
+    plugins = {
+      marks = false,
+      spelling = false,
+    },
   },
   config = function(_, opts)
     require("which-key").setup(opts)
-    -- require("which-key").setup({})
     require("config.keys")
   end,
   keys = {
