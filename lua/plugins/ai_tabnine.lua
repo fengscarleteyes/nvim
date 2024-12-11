06cf409caa0f7fe56902f72a9b3c33f770431794
fengscarleteyes <15916435511@163.com>
2024-12-11 15:10:42 +0800

    update

--- lua/plugins/ai_tabnine.lua
+++ lua/plugins/ai_tabnine.lua
@@ -20,12 +20,15 @@ return {
   opts = {
     max_lines = 1000,
     max_num_results = 3,
+    codelens_color = { gui = "#00FF00", cterm = 244 },
+    codelens_enabled = true,
     sort = true,
     disable_auto_comment = true,
-    accept_keymap = "<Tab>",
-    dismiss_keymap = "<C-]>",
+    accept_keymap = "<C-]>",
+    dismiss_keymap = "<C-[>",
     debounce_ms = 800,
-    suggestion_color = { gui = "#808080", cterm = 244 },
+    suggestion_color = { gui = "#00FF00", cterm = 244 },
+
     exclude_filetypes = { "TelescopePrompt", "NvimTree" },
     log_file_path = nil, -- absolute path to Tabnine log file
     ignore_certificate_errors = false,
