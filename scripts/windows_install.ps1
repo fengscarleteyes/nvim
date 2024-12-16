88c59f813b025a690e7cd6c647fd7564caed3106
fengscarleteyes <15916435511@163.com>
2024-12-16 22:00:28 +0800

    update

--- scripts/windows_install.ps1
+++ scripts/windows_install.ps1
@@ -1,9 +1,9 @@
 # https://dev.to/kaiwalter/get-neovim-plugins-with-build-processes-working-on-windows-i39
 
 # # 设置代理地址和端口
-# $env:MyProxy = "http://192.168.10.118:7890"
-# $env:HTTP_PROXY = $env:MyProxy
-# $env:HTTPS_PROXY = $env:MyProxy
+$env:MyProxy = "http://192.168.10.118:7890"
+$env:HTTP_PROXY = $env:MyProxy
+$env:HTTPS_PROXY = $env:MyProxy
 
 # $env:NO_PROXY="localhost,127.0.0.1"
 
@@ -14,7 +14,7 @@
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-winget install --id Microsoft.Powershell --source winget
+winget install --id Microsoft.Powershell --source winget --proxy $env:MyProxy
 
 # [CmdletBinding()]
 # param (
@@ -26,38 +26,38 @@ winget install --id Microsoft.Powershell --source winget
 # install NeoVim with WinGet, if not already present on system
 if (!$(Get-Command nvim -ErrorAction SilentlyContinue)) {
     winget install Neovim.Neovim --proxy $env:MyProxy
-    # winget install Neovim.Neovim.Nightly
+    # winget install Neovim.Neovim.Nightly --proxy $env:MyProxy
 }
 
 # install ripgrep
 if (!$(Get-Command rg -ErrorAction SilentlyContinue)) {
-  winget install BurntSushi.ripgrep.MSVC
+  winget install BurntSushi.ripgrep.MSVC --proxy $env:MyProxy
 }
 
 # install fd
 if (!$(Get-Command fd -ErrorAction SilentlyContinue)) {
-  winget install sharkdp.fd
+  winget install sharkdp.fd --proxy $env:MyProxy
 }
 
 # install lazygit
 if (!$(Get-Command lazygit -ErrorAction SilentlyContinue)) {
-  winget install JesseDuffield.lazygit
+  winget install JesseDuffield.lazygit --proxy $env:MyProxy
 }
 
 # install zig
 if (!$(Get-Command zig -ErrorAction SilentlyContinue)) {
-  winget install zig.zig
+  winget install zig.zig --proxy $env:MyProxy
 }
 
 $7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
 # install 7z
 if (!$(Get-Command 7z -ErrorAction SilentlyContinue)) {
-  winget install --id=7zip.7zip -e
+  winget install --id=7zip.7zip -e --proxy $env:MyProxy
 }
 
 # install npm
 if (!$(Get-Command npm -ErrorAction SilentlyContinue)) {
-  winget install -e --id OpenJS.NodeJS
+  winget install -e --id OpenJS.NodeJS --proxy $env:MyProxy
 }
 
 npm install -g tree-sitter-cli
