93689e6a130affe0674314000943c98055ef88df
fengscarleteyes <15916435511@163.com>
2024-12-19 10:39:35 +0800

    update

--- lua/keymaps/leader_f.lua
+++ lua/keymaps/leader_f.lua
@@ -1,12 +1,17 @@
+local neotree_cmd_filesystem =
+  [[<cmd>Neotree source=filesystem reveal=true position=float action=focus toggle=true<CR>]]
+
 --stylua: ignore start
 return {
   { "<leader>f", group = "File | Find" }, -- group
-  { "<leader>fF", mode = { "n" }, "<cmd>Neotree source=filesystem reveal=true position=float action=focus toggle=true<CR>", desc = "NeoTree | Filesystem"    },
-  { "<leader>fo", mode = { "n" }, function() require("oil").toggle_float()                 end,                             desc = "Oil       | toggle"      },
-  { "<leader>fn", mode = { "n" }, function() require("telescope").extensions.nerdy.nerdy() end,                             desc = "telescope | find nerd"   },
-  { "<leader>ff", mode = { "n" }, function() require("telescope.builtin").find_files()     end,                             desc = "telescope | find files"  },
-  { "<leader>fb", mode = { "n" }, function() require("telescope.builtin").buffers()        end,                             desc = "telescope | buffers"     },
-  { "<leader>fg", mode = { "n" }, function() require("telescope.builtin").live_grep()      end,                             desc = "telescope | live grep"   },
-  { "<leader>fh", mode = { "n" }, function() require("telescope.builtin").help_tags()      end,                             desc = "telescope | help tags"   },
+  { "<leader>fF", mode = { "n" }, neotree_cmd_filesystem,                                       desc = "Filesystem neotree" },
+  { "<leader>fo", mode = { "n" }, function() require("oil").toggle_float()                 end, desc = "Filesystem oil"     },
+  { "<leader>fn", mode = { "n" }, function() require("telescope").extensions.nerdy.nerdy() end, desc = "find nerd"          },
+  { "<leader>ff", mode = { "n" }, function() require("telescope.builtin").find_files()     end, desc = "find files"         },
+  { "<leader>fb", mode = { "n" }, function() require("telescope.builtin").buffers()        end, desc = "buffers"            },
+  { "<leader>fg", mode = { "n" }, function() require("telescope.builtin").live_grep()      end, desc = "live grep"          },
+  { "<leader>fh", mode = { "n" }, function() require("telescope.builtin").help_tags()      end, desc = "help tags"          },
+  { "<leader>ft", mode = { "n" }, "<Cmd>TodoTelescope<CR>",                                     desc = "TodoTelescope"      },
+  { "<leader>fT", mode = { "n" }, "<cmd>Trouble diagnostics toggle<cr>",                        desc = "Trouble diagnostis" },
   }
 -- stylua: ignore end
