0b9eb9e1ba26f067000dba8aa65cdbb6e16e9ed3
fengscarleteyes <15916435511@163.com>
2024-12-23 21:54:01 +0800

    update

--- lua/packages/neorg.lua
+++ lua/packages/neorg.lua
@@ -1,8 +1,30 @@
 -- https://github.com/nvim-neorg/neorg/wiki/Setup-Guide
+local function get_sep()
+  local sep = ""
+  if vim.uv.os_uname().sysname == "Windows_NT" then
+    sep = "\\"
+  else
+    sep = "/"
+  end
+  return sep
+end
+
 return {
   "nvim-neorg/neorg",
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
   -- config = true,
-  opts = { load = { ["core.defaults"] = {}, ["core.concealer"] = {} } },
+  opts = {
+    load = {
+      ["core.defaults"] = {},
+      ["core.concealer"] = {},
+      ["core.dirman"] = {
+        config = {
+          workspaces = {
+            notes = vim.fn.stdpath("config") .. get_sep() .. "notes",
+          },
+        },
+      },
+    },
+  },
 }
