da2aa19e1566ef06f6909ac89e847cc1cc88ef94
fengscarleteyes <15916435511@163.com>
2024-12-26 08:55:11 +0800

    update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,20 +2,25 @@ return {
   "williamboman/mason-lspconfig.nvim",
   opts = {
     ensure_installed = {
-      formater = {
-        "shfmt", -- bash formater
-        "stylua", -- lua formater
-      },
-      lsp = {
-        "bashls", -- lsp
-        "lua_ls", -- lsp
-        "pyright", -- lsp
-        "ruff", -- linter & formater
-        "taplo", -- toml lsp formater
-        "biome", -- js ts jsx json css GraphQL linter formater
-      },
+      "lua_ls", -- lsp
+      "pyright", -- lsp
+      "ruff", -- linter & formater & lsp
+      "taplo", -- toml lsp & formater
+      "biome", -- js ts jsx json css GraphQL linter & formater & lsp
     },
     automatic_installation = true,
   },
   dependencies = { { "williamboman/mason.nvim", config = true } },
 }
+
+-- [[
+TODO:
+local m = require("mason-registry")
+if not m.is_installed("stylua") and m.has_package("stylua") then
+	print(m.get_installed_package_names())
+	vim.cmd([[<CMD>MasonInstall stylua<CR>]])
+else
+	print(1)
+end
+]]
+
