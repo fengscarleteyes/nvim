a76991f77d093661170079de69a458132ac5f0b0
fengscarleteyes <15916435511@163.com>
2024-12-27 17:31:50 +0800

    update

--- lua/keymaps/no_leader.lua
+++ lua/keymaps/no_leader.lua
@@ -6,11 +6,11 @@ return {
   { "<C-k>",  mode = { "i" },           "<C-o>k",                                            desc = "Up"                  },
   { "<C-j>",  mode = { "i" },           "<C-o>j",                                            desc = "Down"                },
   { "<A-t>",  mode = { "n", "t" },      function() require("FTerm").toggle()            end, desc = "FTerm toggle"        },
+  -- TODO: move this to leader keys?
   { "s",      mode = { "n", "x", "o" }, function() require("flash").jump()              end, desc = "Flash"               },
   { "S",      mode = { "n", "x", "o" }, function() require("flash").treesitter()        end, desc = "Flash Treesitter"    },
   { "r",      mode = { "o"           }, function() require("flash").remote()            end, desc = "Remote Flash"        }, -- in omap"y, d, c ..."
   { "R",      mode = { "o", "x"      }, function() require("flash").treesitter_search() end, desc = "Treesitter Search"   },
   { "<c-s>",  mode = { "c"           }, function() require("flash").toggle()            end, desc = "Toggle Flash Search" }, -- in "/" search mode toggle flash
-
 }
 -- stylua: ignore end
