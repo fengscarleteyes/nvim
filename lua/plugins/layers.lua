a1de53c908ed2de67858e768f8cb234a7795af72
fengscarleteyes <15916435511@163.com>
2024-12-28 21:46:22 +0800

    update

---
+++ lua/plugins/layers.lua
@@ -0,0 +1,34 @@
+-- TODO:
+-- DEBUG_MODE = Layers.mode.new() -- global, accessible from anywhere
+-- DEBUG_MODE:auto_show_help()
+-- DEBUG_MODE:keymaps({
+--   n = {
+--     {
+--       "s",
+--       function()
+--         vim.notify("s")
+--       end,
+--       { desc = "s kb" },
+--     },
+--     {
+--       "c",
+--       function()
+--         vim.notify("c")
+--       end,
+--       { desc = "c kb" },
+--     },
+--     {
+--       "<esc>",
+--       function()
+--         DEBUG_MODE:deactivate()
+--       end,
+--       { desc = "exit" },
+--     },
+--   },
+-- })
+-- DEBUG_MODE:activate()
+
+return {
+  "debugloop/layers.nvim",
+  opts = {}, -- see :help Layers.config
+}
