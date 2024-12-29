1e04c7f204fe7b2df604d969a17a7e81f02c88e4
fengscarleteyes <15916435511@163.com>
2024-12-29 21:10:23 +0800

    update

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,6 +5,16 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    completion = {
+      ghost_text = { enabled = true },
+      menu = {
+        border = "single",
+        draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } } },
+      },
+      documentation = { window = { border = "single" }, auto_show = true, auto_show_delay_ms = 50 },
+      -- list = { selection = "auto_insert" },
+    },
+    signature = { enabled = true },
     keymap = {
       -- https://cmp.saghen.dev/configuration/keymap.html#presets
       preset = "super-tab",
@@ -18,6 +28,7 @@ return {
       -- <C-n>     | select_next"
       -- <C-b>     | scroll_documentation_up"
       -- <C-f>     | scroll_documentation_down"
+      -- cmdline = {}, -- default Disable cmdline completions
       cmdline = {
         preset = "super-tab",
       },
@@ -27,7 +38,7 @@ return {
       nerd_font_variant = "mono",
     },
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "fittencode" },
       providers = {
         fittencode = {
           name = "fittencode",
