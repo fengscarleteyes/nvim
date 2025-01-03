5f1f71a50550d4939aa0d6ea56933cc7ee57fb49
fengscarleteyes <15916435511@163.com>
2025-01-03 17:14:25 +0800

    update

---
+++ lua/plugins/llm.lua
@@ -0,0 +1,56 @@
+-- systemctl stop ollama
+local function local_llm_streaming_handler(chunk, line, assistant_output, bufnr, winid, F)
+  if not chunk then
+    return assistant_output
+  end
+  local tail = chunk:sub(-1, -1)
+  if tail:sub(1, 1) ~= "}" then
+    line = line .. chunk
+  else
+    line = line .. chunk
+    local status, data = pcall(vim.fn.json_decode, line)
+    if not status or not data.message.content then
+      return assistant_output
+    end
+    assistant_output = assistant_output .. data.message.content
+    F.WriteContent(bufnr, winid, data.message.content)
+    line = ""
+  end
+  return assistant_output
+end
+
+local function local_llm_parse_handler(chunk)
+  local assistant_output = chunk.message.content
+  return assistant_output
+end
+
+return {
+  {
+    "Kurama622/llm.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
+    cmd = { "LLMSesionToggle", "LLMSelectedTextHandler" },
+    config = function()
+      local tools = require("llm.common.tools")
+      require("llm").setup({
+        url = "http://localhost:11434/api/chat", -- your url
+        model = "qwen2.5-coder:0.5b",
+
+        streaming_handler = local_llm_streaming_handler,
+        app_handler = {
+          WordTranslate = {
+            handler = tools.flexi_handler,
+            prompt = "Translate the following text to Chinese, please only return the translation",
+            opts = {
+              parse_handler = local_llm_parse_handler,
+              exit_on_move = true,
+              enter_flexible_window = false,
+            },
+          },
+        },
+      })
+    end,
+    keys = {
+      { "<leader>ac", mode = "n", "<cmd>LLMSessionToggle<cr>" },
+    },
+  },
+}
