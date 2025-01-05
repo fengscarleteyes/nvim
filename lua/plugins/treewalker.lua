aa6045a6e8833d799b42f46e790b052a04cb58e8
fengscarleteyes <15916435511@163.com>
2025-01-05 15:19:06 +0800

    update

--- lua/plugins/treewalker.lua
+++ lua/plugins/treewalker.lua
@@ -6,7 +6,8 @@ return {
     highlight_group = "CursorLine",
   },
 }
---
+
+--TODO: congig keymaps
 -- -- movement
 -- vim.keymap.set({ 'n', 'v' }, '<C-k>', '<cmd>Treewalker Up<cr>', { silent = true })
 -- vim.keymap.set({ 'n', 'v' }, '<C-j>', '<cmd>Treewalker Down<cr>', { silent = true })
