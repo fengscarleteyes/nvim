897f6375b5f8c17c3c227ef8178ac5bb822ceb0e
fengscarleteyes <15916435511@163.com>
2025-01-08 22:23:41 +0800

    update

--- lua/plugins/lspui.lua
+++ lua/plugins/lspui.lua
@@ -1,7 +1,11 @@
 return {
   "jinzhongjia/LspUI.nvim",
+  -- enabled = false,
   branch = "main",
+  dependencies = { "neovim/nvim-lspconfig" },
   config = function()
     require("LspUI").setup({})
   end,
 }
+-- TODO:
+-- https://github.com/DNLHC/glance.nvim
