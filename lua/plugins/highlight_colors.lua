af77f661b5aea220053e6f0e009fe9f37cf46521
fengscarleteyes <15916435511@163.com>
2025-01-09 15:40:52 +0800

    update

--- lua/plugins/highlight_colors.lua
+++ lua/plugins/highlight_colors.lua
@@ -1,4 +1,11 @@
 -- https://github.com/brenoprata10/nvim-highlight-colors
 -- TODO: add keys to toggle highlight
-return { "brenoprata10/nvim-highlight-colors", config = true }
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    render = "virtual",
+    virtual_symbol = "■",
+    enable_tailwind = true,
+  },
+}
 -- #00FF00
