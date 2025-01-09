58f5804d3710207ae25323a7182de8cfc4abb5b2
fengscarleteyes <15916435511@163.com>
2025-01-09 16:07:58 +0800

    update

--- lua/keymaps/leader_l.lua
+++ lua/keymaps/leader_l.lua
@@ -1,15 +1,11 @@
-local function lsp_readme()
-  vim.lsp.buf.rename()
-end
 --stylua: ignore start
 return {
   { "<leader>l", group = "Lsp" },
-  { "<leader>lo", mode = { "n" }, "<Cmd>Outline<CR>",      desc = "toggle outline", noremap = true, silent = false },
-  { "<leader>lr", mode = { "n" }, lsp_readme, desc = "Rename",         noremap = true, silent = false },
-  -- TODO: add lsp formatting keymaps & conform.nvim keymaps : LspUI
--- vim.keymap.set('n', 'gD', '<CMD>Glance definitions<CR>')
--- vim.keymap.set('n', 'gR', '<CMD>Glance references<CR>')
--- vim.keymap.set('n', 'gY', '<CMD>Glance type_definitions<CR>')
--- vim.keymap.set('n', 'gM', '<CMD>Glance implementations<CR>')
+  { "<leader>lo", mode = { "n" }, "<Cmd>Outline<CR>",                 desc = "toggle outline",          noremap = true, silent = false },
+  { "<leader>lr", mode = { "n" }, vim.lsp.buf.rename,                 desc = "Rename",                  noremap = true, silent = false },
+  { "<leader>lD", mode = { "n" }, "<CMD>Glance definitions<CR>",      desc = "Glance definitions",      noremap = true, silent = false },
+  { "<leader>lR", mode = { "n" }, "<CMD>Glance references<CR>",       desc = "Glance references",       noremap = true, silent = false },
+  { "<leader>lY", mode = { "n" }, "<CMD>Glance type_definitions<CR>", desc = "Glance type_definitions", noremap = true, silent = false },
+  { "<leader>lM", mode = { "n" }, "<CMD>Glance implementations<CR>",  desc = "Glance implementations",  noremap = true, silent = false },
   }
 -- stylua: ignore end
