12539f1bbb96ee4140abdfe8e331b97ba80c3a09
fengscarleteyes <15916435511@163.com>
2025-01-14 10:17:53 +0800

    update

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,7 +2,7 @@
 return {
   {
     "yetone/avante.nvim",
-    enabled = false,
+    -- enabled = false,
     event = "VeryLazy",
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
@@ -12,10 +12,10 @@ return {
       openai = {
         endpoint = "https://api.deepseek.com/v1",
         model = "deepseek-chat",
+        api_key = "sk-9a7aa1e7ade14eb5806f5604c5a7ffac",
         timeout = 30000, -- Timeout in milliseconds
         temperature = 0,
         max_tokens = 4096,
-        ["local"] = false,
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -28,24 +28,6 @@ return {
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
       {
         -- Make sure to set this up properly if you have lazy=true
         "MeanderingProgrammer/render-markdown.nvim",
