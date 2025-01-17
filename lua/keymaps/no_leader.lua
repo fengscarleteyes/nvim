6d4ad3dc72f7e92b4ba42278c3b5e9a909ada950
fengscarleteyes <15916435511@163.com>
2025-01-17 22:15:12 +0800

    update

--- lua/keymaps/no_leader.lua
+++ lua/keymaps/no_leader.lua
@@ -1,20 +1,19 @@
 -- stylua: ignore start
 return {
   -- Fittencode keymaps
-  { "<leader>a", group = "Ai" },
-  { "<C-]>",      mode = { "i" }, function() require("fittencode").accept_all_suggestions() end,     desc = "Fittencode accept all suggestions" },
-  { "<C-e>",      mode = { "i" }, function() require("fittencode").dismiss_suggestions()    end,     desc = "Fittencode dismiss suggestions"    },
-  { "<C-Right>",  mode = { "i" }, function() require("fittencode").accept_word()            end,     desc = "Fittencode accept word"            },
-  { "<C-Down>",   mode = { "i" }, function() require("fittencode").accept_line()            end,     desc = "Fittencode accept libe"            },
-  { "<C-Left>",   mode = { "i" }, function() require("fittencode").revoke_word()            end,     desc = "Fittencode revoke word"            },
-  { "<C-Up>",     mode = { "i" }, function() require("fittencode").revoke_line()            end,     desc = "Fittencode revoke libe"            },
+  { "<C-]>",      mode = { "i" },      function() require("fittencode").accept_all_suggestions() end,     desc = "Fittencode accept all suggestions" },
+  { "<C-e>",      mode = { "i" },      function() require("fittencode").dismiss_suggestions()    end,     desc = "Fittencode dismiss suggestions"    },
+  { "<C-Right>",  mode = { "i" },      function() require("fittencode").accept_word()            end,     desc = "Fittencode accept word"            },
+  { "<C-Down>",   mode = { "i" },      function() require("fittencode").accept_line()            end,     desc = "Fittencode accept libe"            },
+  { "<C-Left>",   mode = { "i" },      function() require("fittencode").revoke_word()            end,     desc = "Fittencode revoke word"            },
+  { "<C-Up>",     mode = { "i" },      function() require("fittencode").revoke_line()            end,     desc = "Fittencode revoke libe"            },
 
   -- Editor keymaps
-  { "<C-CR>", mode = { "i" },           "<C-o>o",                                            desc = "new line"            },
-  { "<C-h>",  mode = { "i" },           "<C-o>h",                                            desc = "Left"                },
-  { "<C-l>",  mode = { "i" },           "<C-o>l",                                            desc = "Right"               },
-  { "<C-k>",  mode = { "i" },           "<C-o>k",                                            desc = "Up"                  },
-  { "<C-j>",  mode = { "i" },           "<C-o>j",                                            desc = "Down"                },
-  { "<A-t>",  mode = { "n", "t" },      function() require("FTerm").toggle()            end, desc = "FTerm toggle"        },
+  { "<A-t>",      mode = { "n", "t" }, function() require("snacks").terminal.toggle()            end,     desc = "Toggle Terminal"                   },
+  { "<C-CR>",     mode = { "i" },      "<C-o>o",                                                          desc = "new line"                          },
+  { "<C-h>",      mode = { "i" },      "<C-o>h",                                                          desc = "Left"                              },
+  { "<C-l>",      mode = { "i" },      "<C-o>l",                                                          desc = "Right"                             },
+  { "<C-k>",      mode = { "i" },      "<C-o>k",                                                          desc = "Up"                                },
+  { "<C-j>",      mode = { "i" },      "<C-o>j",                                                          desc = "Down"                              },
 }
 -- stylua: ignore end
