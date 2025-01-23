b9ae7aed6055427272259e8b0566b5bab38a3428
fengscarleteyes <15916435511@163.com>
2025-01-23 15:13:25 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,6 +29,11 @@ end
 -- Neovim default updatetime is 4000
 vim.opt.updatetime = 200
 
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
 vim.g.encoding = "utf-8"
 vim.o.fileencoding = "utf-8"
 
