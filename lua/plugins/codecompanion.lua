6d578989501c0b3d66716ec4cde4c7782813206d
fengscarleteyes <15916435511@163.com>
2025-01-26 11:03:48 +0800

    update

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -7,6 +7,7 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      language = "Chinese",
       opts = {
         system_prompt = function()
           return "You are a helpful chinese assistant."
