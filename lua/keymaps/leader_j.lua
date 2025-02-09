bf09d0bed7dcf1f2085a69590f81346e749645da
fengscarleteyes <15916435511@163.com>
2025-02-09 14:35:19 +0800

    update

--- lua/keymaps/leader_j.lua
+++ lua/keymaps/leader_j.lua
@@ -1,11 +1,4 @@
 --stylua: ignore start
 return {
-  -- TODO:move to alt + s
-  { "<leader>j", group = "Jump" }, -- group
-  { "<leader>jj", mode = { "n", "x", "o" }, function() require("flash").jump()              end, desc = "Flash"               },
-  { "<leader>jJ", mode = { "n", "x", "o" }, function() require("flash").treesitter()        end, desc = "Flash Treesitter"    },
-  { "<leader>jr", mode = { "o"           }, function() require("flash").remote()            end, desc = "Remote Flash"        }, -- in omap"y, d, c ..."
-  { "<leader>jR", mode = { "o", "x"      }, function() require("flash").treesitter_search() end, desc = "Treesitter Search"   },
-  { "<c-s>",      mode = { "c"           }, function() require("flash").toggle()            end, desc = "Toggle Flash Search" }, -- in "/" search mode toggle flash
   }
 -- stylua: ignore end
