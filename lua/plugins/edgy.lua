97dab7204f198ddbc39d1e4b1ea8d78d2b6cc05e
fengscarleteyes <15916435511@163.com>
2025-02-16 20:39:20 +0800

    update

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -1,7 +1,7 @@
 return {
   "folke/edgy.nvim",
   event = "VeryLazy",
-  enabled = false,
+  -- enabled = false,
   init = function()
     vim.opt.laststatus = 3
     vim.opt.splitkeep = "screen"
@@ -15,7 +15,14 @@ return {
       },
     },
     left = {
-      -- Neo-tree filesystem always takes half the screen height
+      {
+        title = "Neo-Tree",
+        ft = "neo-tree",
+        filter = function(buf)
+          return vim.b[buf].neo_tree_source == "filesystem"
+        end,
+        size = { height = 0.5 },
+      },
     },
   },
 }
