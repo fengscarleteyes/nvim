91b22dce05583a4f7c31ad35e3826df433351d34
fengscarleteyes <15916435511@163.com>
2025-02-18 16:44:33 +0800

    update

--- lua/keymaps/no_leader.lua
+++ lua/keymaps/no_leader.lua
@@ -22,5 +22,15 @@ return {
   { "<A-g>", mode = { "c"           }, function() require("flash").toggle()            end, desc = "Toggle Flash Search" }, -- in "/" search mode toggle flash
 
   -- { "<A-t>",      mode = { "n", "t" }, "<Cmd>ToggleTerm<CR>",                                             desc = "Toggle Terminal"                   },
+  { "<A-1>", mode = { "n" }, "<Cmd>BufferGoto 1<CR>", desc = "BufferGoto 1", noremap = true, silent = true },
+  { "<A-2>", mode = { "n" }, "<Cmd>BufferGoto 2<CR>", desc = "BufferGoto 2", noremap = true, silent = true },
+  { "<A-3>", mode = { "n" }, "<Cmd>BufferGoto 3<CR>", desc = "BufferGoto 3", noremap = true, silent = true },
+  { "<A-4>", mode = { "n" }, "<Cmd>BufferGoto 4<CR>", desc = "BufferGoto 4", noremap = true, silent = true },
+  { "<A-5>", mode = { "n" }, "<Cmd>BufferGoto 5<CR>", desc = "BufferGoto 5", noremap = true, silent = true },
+  { "<A-6>", mode = { "n" }, "<Cmd>BufferGoto 6<CR>", desc = "BufferGoto 6", noremap = true, silent = true },
+  { "<A-7>", mode = { "n" }, "<Cmd>BufferGoto 7<CR>", desc = "BufferGoto 7", noremap = true, silent = true },
+  { "<A-8>", mode = { "n" }, "<Cmd>BufferGoto 8<CR>", desc = "BufferGoto 8", noremap = true, silent = true },
+  { "<A-9>", mode = { "n" }, "<Cmd>BufferGoto 9<CR>", desc = "BufferGoto 9", noremap = true, silent = true },
+  { "<A-0>", mode = { "n" }, "<Cmd>BufferLast<CR>",   desc = "Buffer Last",  noremap = true, silent = true },
 }
 -- stylua: ignore end
