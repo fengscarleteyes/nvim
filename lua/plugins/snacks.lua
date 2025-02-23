b03731ccb367e63e3b60998272f76293f72c98e5
fengscarleteyes <15916435511@163.com>
2025-02-23 11:25:31 +0800

    update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,7 +7,15 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      sections = {
+        -- { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
