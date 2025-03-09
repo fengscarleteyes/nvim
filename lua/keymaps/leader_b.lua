2c55ea834d5121e90c4cf43854c7e732224d9a2d
fengscarleteyes <15916435511@163.com>
2025-03-09 20:31:40 +0800

    update

--- lua/keymaps/leader_b.lua
+++ lua/keymaps/leader_b.lua
@@ -1,7 +1,6 @@
 --stylua: ignore start
 return {
   { "<leader>b", group = "Buffer" },
-  { "<leader>bl", mode = { "n" }, "<Cmd>BufferList<CR>",                desc = "bufferlist",             noremap = true, silent = true },
   { "<leader>bs", mode = { "n" }, ":lua Snacks.scratch()<CR>",          desc = "Snacks scaratch",        noremap = true, silent = true },
   { "<leader>bS", mode = { "n" }, ":lua Snacks.scratch.select()<CR>",   desc = "Snacks scaratch select", noremap = true, silent = true },
   { "<leader>bb", mode = { "n" }, ":lua Snacks.bufdelete()<CR>",        desc = "BufferClose",            noremap = true, silent = true },
