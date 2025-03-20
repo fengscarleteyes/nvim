8ab3f87cf96fc6e6810cda690da421236ebe1256
fengscarleteyes <15916435511@163.com>
2025-03-20 09:09:39 +0800

    update

--- lua/keymaps/leader_g.lua
+++ lua/keymaps/leader_g.lua
@@ -4,6 +4,6 @@ return {
   -- { "<leader>gb", mode = { "n" }, ":lua Snacks.git.blame_line()<CR>",    desc = "Snacks git blame line"  },
   -- { "<leader>gr", mode = { "n" }, ":lua Snacks.git.get_root()<CR>",      desc = "Snacks git get root"    },
   -- { "<leader>gB", mode = { "n" }, ":lua Snacks.gitbrowse()<CR>",         desc = "Snacks git browse"      },
-  -- { "<leader>gl", mode = { "n" }, ":lua Snacks.lazygit()<CR>",           desc = "Snacks git lazygit"     },
+  { "<leader>gl", mode = { "n" }, ":lua Snacks.lazygit()<CR>",           desc = "Snacks git lazygit"     },
   }
 -- stylua: ignore end
