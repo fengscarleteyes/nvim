9b2edb73859f5a458dbdc3ec777147aa3b61cc0d
fengscarleteyes <15916435511@163.com>
2025-04-21 09:34:42 +0800

    update

--- lua/keymaps/leader_f.lua
+++ lua/keymaps/leader_f.lua
@@ -1,16 +1,12 @@
-local neotree_cmd_filesystem =
-  [[<cmd>Neotree source=filesystem reveal=true position=float action=focus toggle=true<CR>]]
-
 --stylua: ignore start
 return {
   { "<leader>f", group = "File | Find" }, -- group
-  { "<leader>fn", mode = { "n"      }, neotree_cmd_filesystem,                         desc = "Filesystem neotree" },
-  { "<leader>fe", mode = { "n"      }, "<Cmd>Oil<CR>",                                 desc = "Filesystem oil"     },
+  { "<leader>fe", mode = { "n"      }, "<cmd>Neotree filesystem toggle<CR>",           desc = "Filesystem neotree" },
+  { "<leader>fo", mode = { "n"      }, function() require("oil").toggle_float() end,   desc = "Filesystem oil"     },
   { "<leader>ff", mode = { "n"      }, "<Cmd>FzfLua files<CR>",                        desc = "find files"         },
   { "<leader>fc", mode = { "n"      }, "<Cmd>FzfLua colorschemes<CR>",                 desc = "find colorschemes"  },
   { "<leader>fg", mode = { "n"      }, "<Cmd>FzfLua live_grep<CR>",                    desc = "live grep"          },
   { "<leader>fh", mode = { "n"      }, "<Cmd>FzfLua helptags<CR>",                     desc = "help tags"          },
   { "<leader>ft", mode = { "n"      }, "<Cmd>TodoFzfLua<CR>",                          desc = "TodoFzfLua"         },
-  -- { "<leader>fr", mode = { "n", "x" }, function() require("rip-substitute").sub() end, desc = "rip substitute"     },
   }
 -- stylua: ignore end
--- lua/keymaps/no_leader.lua
+++ lua/keymaps/no_leader.lua
@@ -1,12 +1,12 @@
 -- stylua: ignore start
 return {
   -- Fittencode keymaps
-  { "<C-]>",     mode = { "i" }, function() require("fittencode").accept_all_suggestions() end, desc = "Fittencode accept all suggestions" },
-  { "<C-e>",     mode = { "i" }, function() require("fittencode").dismiss_suggestions()    end, desc = "Fittencode dismiss suggestions"    },
-  { "<C-Right>", mode = { "i" }, function() require("fittencode").accept_word()            end, desc = "Fittencode accept word"            },
-  { "<C-Down>",  mode = { "i" }, function() require("fittencode").accept_line()            end, desc = "Fittencode accept libe"            },
-  { "<C-Left>",  mode = { "i" }, function() require("fittencode").revoke_word()            end, desc = "Fittencode revoke word"            },
-  { "<C-Up>",    mode = { "i" }, function() require("fittencode").revoke_line()            end, desc = "Fittencode revoke libe"            },
+  -- { "<C-]>",     mode = { "i" }, function() require("fittencode").accept_all_suggestions() end, desc = "Fittencode accept all suggestions" },
+  -- { "<C-e>",     mode = { "i" }, function() require("fittencode").dismiss_suggestions()    end, desc = "Fittencode dismiss suggestions"    },
+  -- { "<C-Right>", mode = { "i" }, function() require("fittencode").accept_word()            end, desc = "Fittencode accept word"            },
+  -- { "<C-Down>",  mode = { "i" }, function() require("fittencode").accept_line()            end, desc = "Fittencode accept libe"            },
+  -- { "<C-Left>",  mode = { "i" }, function() require("fittencode").revoke_word()            end, desc = "Fittencode revoke word"            },
+  -- { "<C-Up>",    mode = { "i" }, function() require("fittencode").revoke_line()            end, desc = "Fittencode revoke libe"            },
 
   -- Editor keymaps
   { "<C-CR>", mode = { "i" }, "<C-o>o", desc = "new line" },
