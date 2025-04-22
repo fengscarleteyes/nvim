2a8f24c3b306e763f1b888e69c20ea729ecaec5a
fengscarleteyes <15916435511@163.com>
2025-04-22 16:05:16 +0800

    update

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,29 @@
+-- https://github.com/nvimdev/dashboard-nvim
+
+return {
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  config = function()
+    require("dashboard").setup({
+      shortcut_type = "number",
+      theme = "hyper",
+      hide = {
+        statusline = true, -- hide statusline default is true
+        tabline = true, -- hide the tabline
+        winbar = true, -- hide winbar
+      },
+      config = {
+        header = {},
+        shortcut = {
+          -- action can be a function type
+          { desc = "Lazy update", key = "U", action = "Lazy update" },
+        },
+        packages = { enable = true },
+        project = { enable = true, limit = 5 },
+        mru = { enable = true, limit = 10, cwd_only = false },
+        footer = {},
+      },
+    })
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
