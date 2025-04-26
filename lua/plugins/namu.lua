24aa5371ee45b28f9332bab39909b4dc5c72f3d1
fengscarleteyes <15916435511@163.com>
2025-04-26 22:03:44 +0800

    update

--- lua/plugins/namu.lua
+++ lua/plugins/namu.lua
@@ -1,31 +1,30 @@
 -- https://github.com/bassamsdata/namu.nvim
 
-
 -- TODO: test
 return {
-    "bassamsdata/namu.nvim",
-    -- enabled = false,
-    config = function()
-      require("namu").setup({
-        -- Enable the modules you want
-        namu_symbols = {
-          enable = true,
-          options = {}, -- here you can configure namu
-        },
-        -- Optional: Enable other modules if needed
-        ui_select = { enable = false }, -- vim.ui.select() wrapper
-      })
-      -- === Suggested Keymaps: ===
-      vim.keymap.set("n", "<leader>ss",":Namu symbols<cr>" , {
-        desc = "Jump to LSP symbol",
-        silent = true,
-      })
-      vim.keymap.set("n", "<leader>sw", ":Namu workspace<cr>", {
-        desc = "LSP Symbols - Workspace",
-        silent = true,
-      })
-    end,
-  }
+  "bassamsdata/namu.nvim",
+  -- enabled = false,
+  config = function()
+    require("namu").setup({
+      -- Enable the modules you want
+      namu_symbols = {
+        enable = true,
+        options = {}, -- here you can configure namu
+      },
+      -- Optional: Enable other modules if needed
+      ui_select = { enable = false }, -- vim.ui.select() wrapper
+    })
+    -- -- === Suggested Keymaps: ===
+    -- vim.keymap.set("n", "<leader>ss",":Namu symbols<cr>" , {
+    --   desc = "Jump to LSP symbol",
+    --   silent = true,
+    -- })
+    -- vim.keymap.set("n", "<leader>sw", ":Namu workspace<cr>", {
+    --   desc = "LSP Symbols - Workspace",
+    --   silent = true,
+    -- })
+  end,
+}
 --   <CR>	选择项目
 --   <Esc>	关闭选取器
 --   <C-n>	下一项
@@ -38,3 +37,4 @@ return {
 --   <C-v>	垂直分割中打开元件
 --   <C-h>	在水平分割中打开元件
 --   <C-o>	向 CodeCompanion 聊天添加符号
+
