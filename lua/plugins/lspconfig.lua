14b2dcd3876202f9010bdc1983a7a53142c94996
fengscarleteyes <15916435511@163.com>
2025-05-22 10:56:47 +0800

    update

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,6 +4,7 @@ return {
   "neovim/nvim-lspconfig",
   event = "VeryLazy",
   dependencies = {
+    "folke/lazydev.nvim",
     "saghen/blink.cmp",
     "williamboman/mason.nvim",
     -- "williamboman/mason-lspconfig.nvim",
@@ -31,7 +32,6 @@ return {
             if
               path ~= vim.fn.stdpath("config")
               and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")) ---@diagnostic disable-line
-              -- and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
             then
               return
             end
@@ -39,19 +39,33 @@ return {
 
           client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
             runtime = {
+              -- Tell the language server which version of Lua you're using (most
+              -- likely LuaJIT in the case of Neovim)
               version = "LuaJIT",
+              -- Tell the language server how to find Lua modules same way as Neovim
+              -- (see `:h lua-module-load`)
+              path = {
+                "lua/?.lua",
+                "lua/?/init.lua",
+              },
             },
+            -- Make the server aware of Neovim runtime files
             workspace = {
               checkThirdParty = false,
               library = {
                 vim.env.VIMRUNTIME,
-                vim.api.nvim_get_runtime_file("", true),
-                vim.fn.expand("$VIMRUNTIME/lua"),
-                -- Depending on the usage, you might want to add additional paths here.
-                "${3rd}/luv/library",
-                -- "${3rd}/busted/library",
+                -- Depending on the usage, you might want to add additional paths
+                -- here.
+                -- '${3rd}/luv/library'
+                -- '${3rd}/busted/library'
               },
-              -- library = vim.api.nvim_get_runtime_file("", true),
+              -- Or pull in all of 'runtimepath'.
+              -- NOTE: this is a lot slower and will cause issues when working on
+              -- your own configuration.
+              -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+              -- library = {
+              --   vim.api.nvim_get_runtime_file('', true),
+              -- }
             },
           })
         end,
