405688fb103a5d986b1ac761e67a507345b527ce
fengscarleteyes <15916435511@163.com>
2025-05-29 15:55:03 +0800

    updateupdate

--- lua/plugins/lua-console.lua
+++ lua/plugins/lua-console.lua
@@ -3,7 +3,11 @@ return {
   lazy = true,
   keys = {
     { "`", desc = "Lua-console - toggle" },
-    { "<Leader>`", desc = "Lua-console - attach to buffer" },
+    -- { "<Leader>`", desc = "Lua-console - attach to buffer" },
   },
+  -- opts = { preserve_context = false, clear_before_eval = true, notify_result = true },
   opts = {},
+  config = function(_, opts)
+    require("lua-console").setup(opts)
+  end,
 }
