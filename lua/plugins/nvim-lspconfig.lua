ab17060fa3ac12104098abc06103834e06048d8e
fengscarleteyes <15916435511@163.com>
2025-06-03 09:51:30 +0800

    update lsp config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,28 @@
 -- https://github.com/neovim/nvim-lspconfig
 
-vim.lsp.enable({
-  "taplo",
-  "pyright",
-  "ty",
-  "ruff",
-  "lua_ls",
-})
+local function enable_lsp_server(path)
+  local handle = vim.uv.fs_scandir(path)
+  if not handle then
+    return
+  end
+
+  while true do
+    local name, _ = vim.uv.fs_scandir_next(handle)
+    if not name then
+      break
+    end
+
+    if name:match("%.lua$") then
+      local module_name = name:gsub(".lua$", "")
+      vim.lsp.enable(module_name)
+    end
+  end
+end
+
+enable_lsp_server(vim.fn.stdpath("config") .. "/lsp")
 
 return {
   "neovim/nvim-lspconfig",
   dependencies = "folke/lazydev.nvim",
   event = "BufEnter",
 }
-
