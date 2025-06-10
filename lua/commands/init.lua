17d5f4c50ba25ad8303c8089ec93d7892652862a
fengscarleteyes <15916435511@163.com>
2025-06-10 21:04:59 +0800

    update command

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -1,4 +1,4 @@
-local function clean_nvim_state_dir()
+local function remove_state_dir()
   local state_dir = vim.fn.stdpath("state")
   if vim.fn.isdirectory(state_dir) == 1 then
     vim.fn.delete(state_dir, "rf")
@@ -6,9 +6,43 @@ local function clean_nvim_state_dir()
 end
 
 vim.api.nvim_create_user_command(
-  "CleanNvimStateDir", -- command name
-  clean_nvim_state_dir,
+  "RemoveStateDir", -- command name
+  function()
+    vim.ui.input(
+      { prompt = 'RemoveStateDir Enter "Y/y" or "N/n": ' }, -- opts
+      function(input) -- comfirm
+        if string.lower(input) == "y" then
+          remove_state_dir()
+          vim.cmd("qall!") -- 强制退出（不保存）
+        end
+      end
+    )
+  end,
   { desc = "clean nvim state directory" }
+)
+
+local function remove_shada_dir()
+  local state_dir = vim.fn.stdpath("state")
+  local shada_dir = state_dir .. "/shada" -- windows
+  if vim.fn.isdirectory(shada_dir) == 1 then
+    vim.fn.delete(shada_dir, "rf")
+  end
+end
+
+vim.api.nvim_create_user_command(
+  "RemoveShadaDir", -- command name
+  function()
+    vim.ui.input(
+      { prompt = 'RemoveShadaDir Enter "Y/y" or "N/n": ' }, -- opts
+      function(input) -- comfirm
+        if string.lower(input) == "y" then
+          remove_shada_dir()
+          vim.cmd("qall!") -- 强制退出（不保存）
+        end
+      end
+    )
+  end,
+  { desc = "clean nvim shada directory" }
 )
 
 -- 默认打开为新的tab
