de31f2cc576ef3bbf76af184ae595f3338348a50
fengscarleteyes <15916435511@163.com>
2025-07-07 21:21:27 +0800

    update

--- test.py
+++ test.py
@@ -2,9 +2,22 @@ from pathlib import Path
 
 print(__file__)
 
+# asdasd = 123123
+
+
+def asdasd() -> bool:
+  return True
+
+
+asdasd()
+
 
 def test():
-    return Path(__file__)
+  return Path(__file__)
+
+
+def test_4(p):
+  return p if p else None
 
 
 a: Path = Path(__file__)
@@ -15,13 +28,13 @@ print(b.cwd())
 
 
 def test_2() -> bool:
-    return True
+  return True
 
 
 def test_3() -> bool:
-    return False
+  return False
 
 
 if __name__ == "__main__":
-    print(test_2())
-    print(test_3())
+  print(test_2())
+  print(test_3())
