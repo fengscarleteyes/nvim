ba1acd90c2b14c67a5ddb9e6cd1921240ee37478
fengscarleteyes <15916435511@163.com>
2025-09-28 11:11:13 +0800

    add colorscheme

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -1,12 +1,45 @@
+-- colorscheme
+
 return {
+  {
+    "y3owk1n/base16-pro-max.nvim",
+    -- enabled = false,
+    config = function()
+      require("base16-pro-max").setup({
+        colors = {
+          base00 = "#24273a",
+          base01 = "#1e2030",
+          base02 = "#363a4f",
+          base03 = "#494d64",
+          base04 = "#5b6078",
+          base05 = "#cad3f5",
+          base06 = "#f4dbd6",
+          base07 = "#b7bdf8",
+          base08 = "#ed8796",
+          base09 = "#f5a97f",
+          base0A = "#eed49f",
+          base0B = "#a6da95",
+          base0C = "#8bd5ca",
+          base0D = "#8aadf4",
+          base0E = "#c6a0f6",
+          base0F = "#f0c6c6",
+        },
+        -- Plugin integrations
+        plugins = {
+          enable_all = false, -- Enable all supported plugins
+        },
+      })
+      vim.cmd.colorscheme("base16-pro-max")
+    end,
+  },
   {
     "thesimonho/kanagawa-paper.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-    config = function()
-      vim.cmd([[colorscheme kanagawa-paper]])
-    end,
+    -- config = function()
+    --   vim.cmd([[colorscheme kanagawa-paper]])
+    -- end,
   },
 
   {
