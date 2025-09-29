0b82e7bc27dfe82cff9477c51b37481e51376ad5
fengscarleteyes <15916435511@163.com>
2025-09-29 10:22:58 +0800

    add theme

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -2,52 +2,22 @@
 
 return {
   {
-    "y3owk1n/base16-pro-max.nvim",
-    -- enabled = false,
-    config = function()
-      require("base16-pro-max").setup({
-        colors = {
-          base00 = "#24273a",
-          base01 = "#1e2030",
-          base02 = "#363a4f",
-          base03 = "#494d64",
-          base04 = "#5b6078",
-          base05 = "#cad3f5",
-          base06 = "#f4dbd6",
-          base07 = "#b7bdf8",
-          base08 = "#ed8796",
-          base09 = "#f5a97f",
-          base0A = "#eed49f",
-          base0B = "#a6da95",
-          base0C = "#8bd5ca",
-          base0D = "#8aadf4",
-          base0E = "#c6a0f6",
-          base0F = "#f0c6c6",
-        },
-        -- Plugin integrations
-        plugins = {
-          enable_all = false, -- Enable all supported plugins
-        },
-      })
-      vim.cmd.colorscheme("base16-pro-max")
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    -- tag = "vX.0.0",
+    opts = {
+      theme = {
+        style = "dark", --  "dark" | "light"
+        contrast = "high", -- "default" | "high"
+        transparent = false, -- true | false
+      },
+    },
+    config = function(_, opts)
+      require("flow").setup(opts)
+      vim.cmd("colorscheme flow")
     end,
   },
-  {
-    "thesimonho/kanagawa-paper.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    -- config = function()
-    --   vim.cmd([[colorscheme kanagawa-paper]])
-    -- end,
-  },
-
-  {
-    "rmehri01/onenord.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-
   {
     "folke/tokyonight.nvim",
     lazy = false,
