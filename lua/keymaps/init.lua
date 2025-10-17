6f36de254343a8991eeaa9f28ae1544e54bf9761
fengscarleteyes <15916435511@163.com>
2025-10-17 09:11:31 +0800

    add fzfnerdfont cmd in keymap

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -14,6 +14,19 @@ local keymaps = {
 
   {
     "n",
+    "<leader>fn",
+    "<cmd>FzfNerdfont<CR>",
+    {
+      noremap = true, -- 是否禁用递归映射（推荐设为 true，避免无限循环）
+      silent = true, -- 是否静默执行（不显示命令）
+      nowait = false, -- 是否立即应用映射，不等待可能的更长匹配
+      expr = false, -- 是否将 rhs 视为表达式（VimScript）
+      desc = "Neotree", --映射的描述
+    },
+  },
+
+  {
+    "n",
     "<leader>ff",
     "<Cmd>FzfLua files<CR>",
     {
