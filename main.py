1a2a026ab85541f78054ae60685cf7c4b4ae6cf6
fengscarleteyes <15916435511@163.com>
2025-10-17 17:23:52 +0800

    update

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 
 
 def main() -> None:
-    myproxy: str = "http://192.168.43.1:7890"
     # myproxy :str = "http://192.168.10.118:7890"
+    myproxy: str = "http://192.168.43.1:7890"
+    mylocal: str = "localhost,127.0.0.1"
 
-    os.environ["NO_PROXY"] = "localhost,127.0.0.1"
+    os.environ["NO_PROXY"] = mylocal
     os.environ["HTTP_PROXY"] = myproxy
     os.environ["HTTPS_PROXY"] = myproxy
     os.environ["ALL_PROXY"] = myproxy
