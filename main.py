71e660410079939f7cefe691763a543ce9104ad4
fengscarleteyes <15916435511@163.com>
2025-10-17 17:06:06 +0800

    update

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+
+def main() -> None:
+    myproxy: str = "http://192.168.43.1:7890"
+    # myproxy :str = "http://192.168.10.118:7890"
+
+    os.environ["NO_PROXY"] = "localhost,127.0.0.1"
+    os.environ["HTTP_PROXY"] = myproxy
+    os.environ["HTTPS_PROXY"] = myproxy
+    os.environ["ALL_PROXY"] = myproxy
+    # export SOCKET_PROXY=socket5://192.168.43.1:7890
+    # export SOCKET5_PROXY=socket5://192.168.43.1:7890
+    os.system("nvim")
+
+
+if __name__ == "__main__":
+    main()
