ce886d8fd77cb968aa7481242c7bb4687a6ad245
fengscarleteyes <15916435511@163.com>
2025-10-20 11:55:53 +0800

    update

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,6 +1,17 @@
 -- https://github.com/kylechui/nvim-surround
 
 -- TODO: keymaps
+-- <Plug>(nvim-surround-insert)
+-- <Plug>(nvim-surround-insert-line)
+-- <Plug>(nvim-surround-normal)
+-- <Plug>(nvim-surround-normal-cur)
+-- <Plug>(nvim-surround-normal-line)
+-- <Plug>(nvim-surround-normal-cur-line)
+-- <Plug>(nvim-surround-visual)
+-- <Plug>(nvim-surround-visual-line)
+-- <Plug>(nvim-surround-delete)
+-- <Plug>(nvim-surround-change)
+-- <Plug>(nvim-surround-change-line)
 
 return {
   "kylechui/nvim-surround",
