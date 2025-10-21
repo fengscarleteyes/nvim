6fad137cbbac70da85ec1973c911a2ffcc9cd677
fengscarleteyes <15916435511@163.com>
2025-10-21 10:43:20 +0800

    update

--- lua/commands/init.lua
+++ lua/commands/init.lua
@@ -51,21 +51,7 @@ vim.api.nvim_create_autocmd(
   {
     group = vim.api.nvim_create_augroup("highlight_yank", {}),
     callback = function()
-      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 500 })
+      vim.highlight.on_yank({ higroup = "IncSearch", timeout = 2000 })
     end,
   }
 )
--- 默认打开为新的tab
--- vim.api.nvim_create_autocmd("BufAdd", {
---   callback = function()
---       -- 检查是否已经有至少一个标签页
---       if vim.fn.tabpagenr("$") == 1 and vim.fn.winnr("$") == 1 then
---           -- 第一个文件已经在初始标签页中打开，不需要做任何操作
---           return
---       end
---       -- 在新标签页中打开
---       vim.cmd("tabedit " .. vim.fn.expand("<afile>"))
---       -- 关闭原来的缓冲区
---       vim.cmd("bdelete #")
---   end
--- })
