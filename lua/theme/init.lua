66e2cc9d88faca56d8fc7cb55858bedcdf407c44
fengscarleteyes <15916435511@163.com>
2026-09-07 17:13:59 +0800

    try new theme

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -4,8 +4,23 @@
 -- require("tokyonight").setup({ transparent = vim.g.transparent_enabled })
 -- vim.cmd("colorscheme tokyonight-night")
 
+-- vim.pack.add({
+--   "https://github.com/mitander/flume.nvim",
+-- })
+-- vim.opt.termguicolors = true
+-- require("flume").setup({ schema = "dusk" })
+
 vim.pack.add({
-  "https://github.com/mitander/flume.nvim",
+  {
+    src = 'https://github.com/ThorstenRhau/token',
+    version = vim.version.range('*'),
+  },
 })
-vim.opt.termguicolors = true
-require("flume").setup({ schema = "dusk" })
+local token = require('token')
+---@type token.Config
+local config = {
+  transparent = false,
+  plugins = { gitsigns = true, snacks = true },
+}
+token.setup(config)
+vim.cmd.colorscheme('token') -- or 'token-ultra', 'token-meridian', 'token-flint', 'token-temper'
